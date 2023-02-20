ea331cf2900da172502e88bcb86ebc977131a41c
Skultty <samuel1skultety@gmail.com>
2023-02-20 08:23:53 +0100

    tailwind css config fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   plugins: {
+    tailwindcss: {
+      content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    },
     autoprefixer: {},
   },
 };
